3905f1c786b471442f02832c8c199a12b97353b5
chrisjsimpson <chris.j.simpson@live.co.uk>
2022-07-11 23:18:35 +0100

    banvoicemessagesbot

---
+++ README.md
@@ -0,0 +1,10 @@
+# banvoicemessages_bot
+
+A telegram bot to ban (and delete) voice messages automatically.
+
+# Locally run
+```
+docker build --load -t app .
+```
+
+http://127.0.0.1:5000
---
+++ app.py
@@ -0,0 +1,52 @@
+from starlette.applications import Starlette
+from starlette.responses import PlainTextResponse
+from starlette.routing import Route
+
+import requests
+from dotenv import load_dotenv
+import os
+
+load_dotenv(verbose=True)
+
+TELEGRAM_API_TOKEN = os.getenv("TELEGRAM_API_TOKEN", None)
+TELEGRAM_CHAT_ID = os.getenv("TELEGRAM_CHAT_ID", None)
+APP_DEBUG = os.getenv("APP_DEBUG", "0")
+
+API_ENDPOINT = "https://api.telegram.org/bot/"
+
+
+async def index(request):
+    body = await request.json()
+    print(body)
+
+    message_id = body["message"]["message_id"]
+    if "voice" in body["message"]:
+        print("Must be a voice message. Lets's delte it!")
+        reply = "We've banned voice messages. Deleting it now."
+        # Warn the user
+        requests.get(
+            f"{API_ENDPOINT}{TELEGRAM_API_TOKEN}/sendMessage?chat_id={TELEGRAM_CHAT_ID}&text={reply}"  # noqa: E501
+        )
+        # Delete their voice message
+        requests.get(
+            f"{API_ENDPOINT}{TELEGRAM_API_TOKEN}/deleteMessage?chat_id={TELEGRAM_CHAT_ID}&message_id={message_id}"  # noqa: E501
+        )
+    return PlainTextResponse("OK")
+
+
+async def health(request):
+    return PlainTextResponse("OK")
+
+
+routes = [
+    Route("/", index, methods=["GET", "POST"]),
+    Route("/health", health),
+]
+
+
+if APP_DEBUG == "1" or APP_DEBUG.lower() == "true":
+    debug = True
+else:
+    debug = False
+
+app = Starlette(debug=debug, routes=routes)
